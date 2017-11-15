7ba57cab4c5efaae76706475f4ff41006b98e9ba
Darwin <darwinjsgithub.services@sanoys.com>
2017-11-15 07:49:52 -0500

    fix

--- DropSysinternalsTools.ps1
+++ DropSysinternalsTools.ps1
@@ -9,6 +9,10 @@ To use a different default tool list, call the code like this:
 Invoke-Expression (invoke-webrequest -uri 'https://raw.githubusercontent.com/DarwinJS/WindowsEscalationToolkit/master/DropSysinternalsTools.ps1') -ToolsToPull procexp.exe,procmon.exe
 
 #>
+param (
+    [string]$ToolsToPull = ("procexp.exe;procmon.exe;autoruns.exe" -split ';'),
+    [string]$TargetFolder = "$env:public"
+)
 
 If (!$ToolsToPull)
 {$ToolsToPull = ("procexp.exe;procmon.exe;autoruns.exe" -split ';')}
