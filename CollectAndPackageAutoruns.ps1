da623c46c423f7f9474f1ca9aec08b06c3bb134f
Darwin <darwinjsgithub.services@sanoys.com>
2017-11-15 07:06:41 -0500

    first tools

---
+++ CollectAndPackageAutoruns.ps1
@@ -0,0 +1,16 @@
+<#
+#Best explanation of why this coded is needed: https://cloudywindows.com/post/automated-collection-of-diagnostic-system-information-from-systems-you-dont-own/
+#Run this directly from this location with: 
+Invoke-Expression (invoke-webrequest -uri 'https://raw.githubusercontent.com/DarwinJS/WindowsEscalationToolkit/master/CollectAndPackageSystemInfo.ps1')
+
+#>
+
+#Autoruns
+If (![bool]([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{$erroractionpreference = 'Stop' ; Throw "You must be an administrator to run this!"}
+Invoke-WebRequest -Uri 'http://live.sysinternals.com/autoruns.exe'  -outfile "$env:public\autoruns.exe"
+cd $env:PUBLIC
+$Filename = "$env:PUBLIC\Autoruns-$env:Computername-$(Get-date -format 'yyMMdd-hhmmss').arn"
+Start-Process "$env:public\autoruns.exe" -ArgumentList "-e -a $Filename" -wait
+Write-Host "Please send this file to the requester: `"$Filename`""
+Write-Host "You can also run `"$env:PUBLIC\autoruns.exe`" and use File => Open to open the file and examine what was collected."
---
+++ DropSysinternalsTools.ps1
@@ -0,0 +1,22 @@
+<#
+#Run this directly from this location with: 
+Invoke-Expression (invoke-webrequest -uri 'https://raw.githubusercontent.com/DarwinJS/WindowsEscalationToolkit/master/DropSysinternalsTools.ps1')
+
+Grabs one or more sysinternals tools and places them in the target folder.
+
+To use a different default tool list, call the code like this:
+
+Invoke-Expression (invoke-webrequest -uri 'https://raw.githubusercontent.com/DarwinJS/WindowsEscalationToolkit/master/DropSysinternalsTools.ps1') -ToolsToPull procexp.exe,procmon.exe
+
+#>
+
+Param
+(
+    [string[]]$ToolsToPull = @('procexp.exe','procmon.exe','autoruns.exe')
+    [string]$TargetFolder = "$env:public"
+)
+
+ForEach ($Tool in $ToolsToPull)
+{
+    Invoke-WebRequest -Uri "http://live.sysinternals.com/$Tool" -outfile "$TargetFolder/$Tool"
+}
