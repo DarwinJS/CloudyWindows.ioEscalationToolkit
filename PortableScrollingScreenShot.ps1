2694f91099ac2c201b31ba0e132282457ffb4ac1
Darwin <DarwinJS@users.noreply.github.com>
2018-03-24 07:58:42 -0400

    Update PortableScrollingScreenShot.ps1

--- PortableScrollingScreenShot.ps1
+++ PortableScrollingScreenShot.ps1
@@ -1,13 +1,7 @@
 <#
 CloudyWindows.io Escalation Toolkit: http://cloudywindows.io
 #Run this directly from this location with: 
-Invoke-Expression (invoke-webrequest -uri 'https://raw.githubusercontent.com/DarwinJS/WindowsEscalationToolkit/master/DropSysinternalsTools.ps1')
-
-Grabs one or more sysinternals tools and places them in the target folder.
-
-To use a different default tool list, call the code like this:
-
-Invoke-Expression (invoke-webrequest -uri 'https://raw.githubusercontent.com/DarwinJS/WindowsEscalationToolkit/master/DropSysinternalsTools.ps1') -ToolsToPull procexp.exe,procmon.exe
+Invoke-Expression (invoke-webrequest -uri 'https://raw.githubusercontent.com/DarwinJS/CloudyWindows.ioEscalationToolkit/master/PortableScrollingScreenShot.ps1')
 
 #>
 Param (
