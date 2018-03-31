860a20c005a5e1e54f0ad0e6f8a1b5b0d16fd80e
Darwin <darwinjsgithub.services@sanoys.com>
2018-03-31 12:55:53 -0400

    Fixes

--- PortableScrollingScreenShotZFP.ps1
+++ PortableScrollingScreenShotZFP.ps1
@@ -8,7 +8,7 @@
   Capturing evidence of breakage and evidence that fixes are effective is a fundamental part of ensuring escalations 
 
   iex (iwr 'https://raw.githubusercontent.com/DarwinJS/WindowsEscalationToolkit/master/PortableScrollingScreenShotZFP.ps1')
-  iwr https://raw.githubusercontent.com/DarwinJS/WindowsEscalationToolkit/master/PortableScrollingScreenShotZFP.ps1' -outfile $env:public\PortableScrollingScreenShotZFP.ps1 ; & $env:public\PortableScrollingScreenShotZFP.ps1 -repeatintervalminutes 1
+  iwr https://raw.githubusercontent.com/DarwinJS/WindowsEscalationToolkit/master/PortableScrollingScreenShotZFP.ps1' -outfile $env:public\PortableScrollingScreenShotZFP.ps1 ; & $env:public\PortableScrollingScreenShotZFP.ps1 -CloudyWindowsToolsSuppressCleanUp -CloudyWindowsToolsRoot "c:\users\public\test"
 .COMPONENT
    CloudyWindows.io
 .ROLE
@@ -17,16 +17,21 @@
 
 Param (
   [String]$CloudyWindowsToolsRoot = "$(If ("$env:CloudyWindowsToolsRoot") {"$env:CloudyWindowsToolsRoot"} else {"$env:public\CloudyWindows.io_EscalationTools"})",
-  [String]$CloudyWindowsToolsSuppressCleanUp = "$(If ("$env:CloudyWindowsToolsSuppressCleanUp" -and "$env:CloudyWindowsToolsSuppressCleanUp" -ilike '*true*') {$True},
+  [String]$CloudyWindowsToolsSuppressCleanUp = $(If ("$env:CloudyWindowsToolsSuppressCleanUp" -and ("$env:CloudyWindowsToolsSuppressCleanUp" -ilike '*true*')) {"True"} else {"False"}),
   [String]$Name = "PicPick ScreenShot",
-  [String]$Description = "Free, Portable, has scrolling screenshots",
+  [String]$Description = "Free, Portable, with screenshots of scrolling windows",
   [String]$EXE = 'picpick.exe',
   [String]$URL = 'http://ngwin.com/download/latest/picpick_portable.zip',
   [String]$SubFolder = 'picpick'
   )
 
-  $LastSegment = (("$URL") -split '/') | select -last 1
-  $CloudyWindowsToolFolder = "$CloudyWindowsToolsRoot\$SubFolder"
+$LastSegment = (("$URL") -split '/') | select -last 1
+$CloudyWindowsToolFolder = "$CloudyWindowsToolsRoot\$SubFolder"
+If ($CloudyWindowsToolsSuppressCleanUp -ilike '*true*') {[Bool]$BoolCloudyWindowsToolsSuppressCleanUp=$True} Else {[Bool]$BoolCloudyWindowsToolsSuppressCleanUp=$False}
+
+If (@(Get-process $exe.replace('.exe','') -EA 0).count -gt 0) {Write-warning "`"$Name`" is already running, exiting..."; exit 0}
+
+write-host "`$CloudyWindowsToolsSuppressCleanUp is $CloudyWindowsToolsSuppressCleanUp"
 
 $ToolBanner = @"
 *****************************************************
@@ -49,14 +54,20 @@ If (!(Test-Path "$CloudyWindowsToolFolder\$EXE"))
   }
 }
 
-If ($CloudyWindowsToolSuppressCleanup)
+$WaitParam=$False
+If (!$BoolCloudyWindowsToolsSuppressCleanUp)
 {
-  Write-Warning "Waiting for $CloudyWindowsToolFolder\$EXE to exit (Check for a tray item if exit did not occur when you expected"
+  Write-Warning "Waiting for $CloudyWindowsToolFolder\$EXE to exit (Check for a tray item if programs does not appear to exit as expected)"
+  $WaitParam = $True
+}
+else 
+{
+  Write-Warning "Please wait while $name is launched..."  
 }
 
-$processhandle = start-process -FilePath "$CloudyWindowsToolFolder\$EXE" -wait -PassThru
+$processhandle = start-process -PassThru -FilePath "$CloudyWindowsToolFolder\$EXE" -wait:$WaitParam
 
-If ($CloudyWindowsToolSuppressCleanup)
+If (!$BoolCloudyWindowsToolsSuppressCleanUp)
 {
   While (!$processhandle.HasExited)
   {
@@ -64,4 +75,8 @@ If ($CloudyWindowsToolSuppressCleanup)
   }
   Write-Host "Zero Footprint cleanup, use switch -CloudyWindowsToolsSuppressCleanUp or Environment Variable CloudyWindowsToolsSuppressCleanUp = True"
   Remove-Item "$CloudyWindowsToolFolder" -Recurse -Force
+}
+else 
+{
+  Write-Host "`"$Name`" is available for reuse in folder `"$CloudyWindowsToolFolder`""
 }
